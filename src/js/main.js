15699c1426fc18aa306dbaa00304e62a39f8252b
Brian O'Toole <brian@sparxoo.com>
2017-12-29 18:10:58 -0500

    improved global styles

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,63 @@ $(document).ready(function() {
     $('#js-nav-toggle').toggleClass('active');
   });
 });
+
+// scroll on click of anchor link (a href="#")
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 900);
+        return false;
+      }
+    }
+  });
+});
+
+// form validation
+function validateRegister() {
+	var email, atpos, dotpos, username;
+
+	username = $('#name').val();
+	email = $('#email').val();
+	atpos = email.indexOf('@');
+	dotpos = email.lastIndexOf('.');
+	message = $('#message').val();
+
+	if (username == null || username == '') {
+	$('#js-form-message').addClass('text-error text-large').text('* Please enter your name.');
+	$('#name').focus();
+	return false;
+	}
+	if(email == null || email == '') {
+	$('#js-form-message').addClass('text-error text-large').text('* Please enter your email.');
+	$('#email').focus();
+	return false;
+	}
+	if(atpos < 1 || dotpos < atpos+2 || dotpos+2 >= email.length) {
+	$('#js-form-message').addClass('text-error text-large').text('* Please enter a valid email address.');
+	$('#email').focus();
+	return false;
+	}
+	if (message == null || message == '') {
+	$('#js-form-message').addClass('text-error text-large').text('* Please enter your message.');
+	$('#message').focus();
+	return false;
+	}
+	else {
+	  var datastring = $("#js-form").serialize();
+	  $.ajax({
+	    //send the form using formspree
+	    url: "https://formspree.io/botoole518@gmail.com", 
+	    method: "POST",
+	    data: datastring,
+	    dataType: "json"
+	  });
+	  $('#js-form-message').addClass('text-success text-large').text('Thanks for reaching out! Your message has been sent successfully. I\'ll be in touch as soon as possible.');
+	  return false;
+	}
+}
