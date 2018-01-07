1895774d6d6d74ba8052f25554ef5bab1361a989
Brian O'Toole <brian@sparxoo.com>
2018-01-06 22:09:20 -0500

    temp fix - weenmax and webpack arent playing nice

--- src/js/tweens.js
+++ src/js/tweens.js
@@ -1,14 +1,14 @@
 //TimelineMax & ScrollMagic
 import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';   
-import { TimelineMax } from 'gsap';
+//import { TimelineMax } from 'gsap';
 // TweenMax
 //tl.method(element, duration, vars), delay
-var tl = new TimelineMax({});
+/*var tl = new TimelineMax({});
 tl
   // news page: timeline 
 	.staggerFrom('.post-item', 0.5, {y: 5,autoAlpha:0,ease: Power1.easeOut}, 0.15)
 	.to(".svg-web-gear", 5, {rotation:360,transformOrigin:"center center", ease:Linear.easeNone, repeat:-1})
-	
+	*/
 
 // ScrollMagic
 // Init
