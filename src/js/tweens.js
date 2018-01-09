260b45e3d0db299d33a7e00e8e1192dbb7042adc
Brian O'Toole <brian@sparxoo.com>
2018-01-09 16:26:45 -0500

    rm tweenmax

--- src/js/tweens.js
+++ src/js/tweens.js
@@ -3,12 +3,13 @@ import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';
 //import { TweenMax } from 'gsap';
 // TweenMax
 //tl.method(element, duration, vars), delay
+/*
 var tl = new TimelineMax({});
 tl
   // news page: timeline 
 	.staggerFrom('.post-item', 0.5, {y: 5,autoAlpha:0,ease: Power1.easeOut}, 0.15)
 	//.to('path.svg-web-gear', 5, {rotation:360,transformOrigin:'50% 50%', ease:Linear.easeNone, repeat:-1})
-	
+*/
 
 // ScrollMagic
 // Init
@@ -24,12 +25,12 @@ var pinIntroScene = new ScrollMagic.Scene({
 .addTo(controller);
 
 //loop through each el and build scene
-$('.project-block, .hero-content').each(function() {
+$('.hero-content').each(function() {
 	//build a scene
 	var projectScene = new ScrollMagic.Scene({
 		//scene options
 		triggerElement: this.children[0],
-		triggerHook: 0.9,
+		triggerHook: 0,
 		reverse: false
 	})
 	.setClassToggle(this, 'fadeIn') //add class to el
